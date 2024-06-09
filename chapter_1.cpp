c3390f4aa78e3213fc918d9672f86b538bac4e56
Mahbubur Rahman <mahbubur9@yahoo.com>
2024-06-09 17:52:58 +0600

    Create chapter_1.cpp

---
+++ chapter_1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// O(N) complexity program
+/*
+int main() {
+    int n;
+    cin >> n;
+    
+    int a[n];
+    for (int i = 0; i < n; i++) { // O(N)
+        cin >> a[i];
+    }
+    
+    int sum = 0;
+    
+    for (int i = 0; i < n; i++) { // O(N)
+        sum = sum + a[i];
+    }
+    
+    cout << sum << endl;
+    
+    return 0;
+} */
+
+
+// i jodi gunitok akare agay or value ta jodi kuno kichu dara vag kore kore jay tahole logarithm complexity
+int main() {
+    
+    
+    return 0;
+}
