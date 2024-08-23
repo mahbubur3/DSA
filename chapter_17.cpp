500538ef78302aaf30ce84b2f52706d2decfee83
Mahbubur Rahman <mahbubur9@yahoo.com>
2024-08-23 15:26:37 +0600

    Update chapter_17.cpp

--- chapter_17.cpp
+++ chapter_17.cpp
@@ -2,39 +2,41 @@
 
 using namespace std;
 
-// Pre order
-class Node {
+// Traverse with Pre order
+class BinaryTreeNode {
 public:
 	int data;
-	Node* left;
-	Node* right;
-	Node(int data) {
+	BinaryTreeNode* left;
+	BinaryTreeNode* right;
+
+	BinaryTreeNode(int data) {
 		this->data = data;
 		this->left = NULL;
 		this->right = NULL;
 	}
 };
 
-void preOrder(Node* root) {
+void preOrder(BinaryTreeNode* root) {
 	if (root == NULL) {
 		return;
 	}
+
 	cout << root->data << " ";
 	preOrder(root->left);
 	preOrder(root->right);
 }
 
 int main() {
-	Node* root = new Node(10);
-	Node* a = new Node(20);
-	Node* b = new Node(30);
-	Node* c = new Node(40);
-	Node* d = new Node(50);
-	Node* e = new Node(60);
-	Node* f = new Node(70);
-	Node* g = new Node(80);
-	Node* h = new Node(90);
-	Node* i = new Node(100);
+	BinaryTreeNode* root = new BinaryTreeNode(10);
+	BinaryTreeNode* a = new BinaryTreeNode(20);
+	BinaryTreeNode* b = new BinaryTreeNode(30);
+	BinaryTreeNode* c = new BinaryTreeNode(40);
+	BinaryTreeNode* d = new BinaryTreeNode(50);
+	BinaryTreeNode* e = new BinaryTreeNode(60);
+	BinaryTreeNode* f = new BinaryTreeNode(70);
+	BinaryTreeNode* g = new BinaryTreeNode(80);
+	BinaryTreeNode* h = new BinaryTreeNode(90);
+	BinaryTreeNode* i = new BinaryTreeNode(100);
 
 	root->left = a;
 	root->right = b;
@@ -53,39 +55,41 @@ int main() {
 
 
 
-// Post order
-class Node {
+// Traverse with post-order
+class BinaryTreeNode {
 public:
 	int data;
-	Node* left;
-	Node* right;
-	Node(int data) {
+	BinaryTreeNode* left;
+	BinaryTreeNode* right;
+
+	BinaryTreeNode(int data) {
 		this->data = data;
 		this->left = NULL;
 		this->right = NULL;
 	}
 };
 
-void postOrder(Node* root) {
+void postOrder(BinaryTreeNode* root) {
 	if (root == NULL) {
 		return;
 	}
+
 	postOrder(root->left);
 	postOrder(root->right);
 	cout << root->data << " ";
 }
 
 int main() {
-	Node* root = new Node(10);
-	Node* a = new Node(20);
-	Node* b = new Node(30);
-	Node* c = new Node(40);
-	Node* d = new Node(50);
-	Node* e = new Node(60);
-	Node* f = new Node(70);
-	Node* g = new Node(80);
-	Node* h = new Node(90);
-	Node* i = new Node(100);
+	BinaryTreeNode* root = new BinaryTreeNode(10);
+	BinaryTreeNode* a = new BinaryTreeNode(20);
+	BinaryTreeNode* b = new BinaryTreeNode(30);
+	BinaryTreeNode* c = new BinaryTreeNode(40);
+	BinaryTreeNode* d = new BinaryTreeNode(50);
+	BinaryTreeNode* e = new BinaryTreeNode(60);
+	BinaryTreeNode* f = new BinaryTreeNode(70);
+	BinaryTreeNode* g = new BinaryTreeNode(80);
+	BinaryTreeNode* h = new BinaryTreeNode(90);
+	BinaryTreeNode* i = new BinaryTreeNode(100);
 
 	root->left = a;
 	root->right = b;
@@ -104,39 +108,41 @@ int main() {
 
 
 
-
-class Node {
+// Traverse with in-order
+class BinaryTreeNode {
 public:
 	int data;
-	Node* left;
-	Node* right;
-	Node(int data) {
+	BinaryTreeNode* left;
+	BinaryTreeNode* right;
+
+	BinaryTreeNode(int data) {
 		this->data = data;
 		this->left = NULL;
 		this->right = NULL;
 	}
 };
 
-void inOrder(Node* root) {
+void inOrder(BinaryTreeNode* root) {
 	if (root == NULL) {
 		return;
 	}
+
 	inOrder(root->left);
 	cout << root->data << " ";
 	inOrder(root->right);
 }
 
 int main() {
-	Node* root = new Node(10);
-	Node* a = new Node(20);
-	Node* b = new Node(30);
-	Node* c = new Node(40);
-	Node* d = new Node(50);
-	Node* e = new Node(60);
-	Node* f = new Node(70);
-	Node* g = new Node(80);
-	Node* h = new Node(90);
-	Node* i = new Node(100);
+	BinaryTreeNode* root = new BinaryTreeNode(10);
+	BinaryTreeNode* a = new BinaryTreeNode(20);
+	BinaryTreeNode* b = new BinaryTreeNode(30);
+	BinaryTreeNode* c = new BinaryTreeNode(40);
+	BinaryTreeNode* d = new BinaryTreeNode(50);
+	BinaryTreeNode* e = new BinaryTreeNode(60);
+	BinaryTreeNode* f = new BinaryTreeNode(70);
+	BinaryTreeNode* g = new BinaryTreeNode(80);
+	BinaryTreeNode* h = new BinaryTreeNode(90);
+	BinaryTreeNode* i = new BinaryTreeNode(100);
 
 	root->left = a;
 	root->right = b;
