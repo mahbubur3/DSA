74294d3ff72e69e96470db4dc35edc8142f85a60
Mahbubur Rahman <mahbubur9@yahoo.com>
2024-10-18 20:15:04 +0600

    Update Algo_chapter_6.cpp

--- Algo_chapter_6.cpp
+++ Algo_chapter_6.cpp
@@ -1,62 +1,47 @@
-#include <bits/stdc++.h>
+// Dijkstra Naive version emplementation (BFS)
+const int N = 100;
+vector<pair<int, int>> v[N]; // one is node, another is cost or weight
+int des[N];
 
-using namespace std;
-
-
-// Dectect cycle in undirected graph using BFS
-const int n = 1e5 + 5;
-bool isVisited[n];
-vector<int> adj[n];
-int parentArray[n];
-bool isCycle;
-
-void bfs(int src) {
-	queue<int> q;
-	q.push(src);
-	isVisited[src] = true;
+void dijkstra(int src) {
+	queue<pair<int, int>> q;
+	q.push({src, 0});
+	des[src] = 0;
 
 	while (!q.empty()) {
-		int parentNode = q.front();
+		pair<int, int> parent = q.front();
 		q.pop();
 
-		for (int childNode : adj[parentNode]) {
-			if (isVisited[childNode] && parentArray[parentNode] != childNode) {
-				isCycle = true;
-			}
-
-			if (!isVisited[childNode]) {
-				isVisited[childNode] = true;
-				parentArray[childNode] = parentNode;
-				q.push(childNode);
+		int parentNode = parent.first, parentCost = parent.second;
+		for (pair<int, int> child : v[parentNode]) {
+			int childNode = child.first, childCost = child.second;
+			if (parentCost + childCost < des[childNode]) {
+				des[childNode] = parentCost + childCost; // Path relax
+				q.push({childNode, des[childNode]});
 			}
 		}
 	}
 }
 
 int main() {
-	int node, edge;
-	cin >> node >> edge;
-	while (edge--) {
-		int x, y;
-		cin >> x >> y;
-		adj[x].push_back(y);
-		adj[y].push_back(x);
+	int n, e;
+	cin >> n >> e;
+	while (e--) {
+		int x, y, z;
+		cin >> x >> y >> z;
+		v[x].push_back({y, z});
+		v[y].push_back({x, z});
 	}
 
-	memset(isVisited, false, sizeof(isVisited));
-	memset(parentArray, false, sizeof(parentArray));
-	isCycle = false;
-
-	for (int i = 0; i < node; i++) {
-		if (!isVisited[i]) {
-			bfs(i);
-		}
+	for (int i = 0; i < n; i++) {
+		des[i] = INT_MAX;
 	}
 
-	if (isCycle) {
-		cout << "Cycle found" << endl;
-	} else {
-		cout << "Cycle not found" << endl;
+	dijkstra(0);
+
+	// Print
+	for (int i = 0; i < n; i++) {
+		cout << i << " - " << des[i] << endl;
 	}
 
 	return 0;
@@ -67,109 +52,59 @@ int main() {
 
 
 
-// Dectect cycle in undirected graph using DFS
-const int n = 1e5 + 5;
-bool isVisited[n];
-vector<int> adj[n];
-int parentArray[n];
-bool isCycle;
 
-void dfs(int parentNode) {
-    isVisited[parentNode] = true;
-    for (int childNode : adj[parentNode]) {
-        if (isVisited[childNode] && parentArray[parentNode] != childNode) {
-			isCycle = true;
+// Dijkstra optimized version emplementation (BFS)
+cont int N = 100;
+vector<pair<int, int>> v[N]; // one is node, another is cost or weight
+int des[N];
+
+class cmp {
+public:
+	bool operator()(pair<int, int> a, pair<int, int> b) {
+		return a.second < b.second;
+	}
+}
+
+void dijkstra(int src) {
+	priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
+	pq.push({src, 0});
+	des[src] = 0;
+	while (!pq.empty()) {
+		pair<int, int> parent = pq.front();
+		pq.pop();
+
+		int parentNode = parent.first, parentCost = parent.second;
+		for (pair<int, int> child : v[parentNode]) {
+			int childNode = child.first, childCost = child.second;
+			
+			if (parentCost + childCost < des[childNode]) {
+				des[childNode] = parentCost + childCost; // Path relax
+				q.push({childNode, des[childNode]});
+			}
 		}
-        
-        if (!isVisited[childNode]) {
-            parentArray[childNode] = parentNode;
-            dfs(childNode);
-        }
-    }
+	}
 }
 
 int main() {
-	int node, edge;
-	cin >> node >> edge;
-	while (edge--) {
-		int x, y;
-		cin >> x >> y;
-		adj[x].push_back(y);
-		adj[y].push_back(x);
+	int n, e;
+	cin >> n >> e;
+	while (e--) {
+		int x, y, z;
+		cin >> x >> y >> z;
+		v[x].push_back({y, z});
+		v[y].push_back({x, z});
 	}
 
-	memset(isVisited, false, sizeof(isVisited));
-	memset(parentArray, false, sizeof(parentArray));
-	isCycle = false;
-
-	for (int i = 0; i < node; i++) {
-		if (!isVisited[i]) {
-			dfs(i);
-		}
+	for (int i = 0; i < n; i++) {
+		des[i] = INT_MAX;
 	}
 
-	if (isCycle) {
-		cout << "Cycle found" << endl;
-	} else {
-		cout << "Cycle not found" << endl;
+	dijkstra(0);
+
+	// Print
+	for (int i = 0; i < n; i++) {
+		cout << i << "->" << des[i] << endl;
 	}
 
-	return 0;
-}
-
-
-
-
-
-
-// Dectect cycle in directed graph using DFS
-const int n = 1e5 + 5;
-bool isVisited[n];
-vector<int> adj[n];
-bool isPathVisited[n];
-bool isCycle;
-
-void dfs(int parentNode) {
-    isVisited[parentNode] = true;
-    isPathVisited[parentNode] = true;
-    
-    for (int childNode : adj[parentNode]) {
-        if (!isPathVisited[childNode]) {
-            isCycle = true;
-        }
-        
-        if (!isVisited[childNode]) {
-            dfs(childNode);
-        }
-    }
-    
-    isPathVisited[parentNode] = false;
-}
-
-int main() {
-	int node, edge;
-	cin >> node >> edge;
-	while (edge--) {
-		int x, y;
-		cin >> x >> y;
-		adj[x].push_back(y);
-	}
-	
-	memset(isVisited, false, sizeof(isVisited));
-	memset(isPathVisited, false, sizeof(isPathVisited));
-	isCycle = false;
-	
-	for (int i = 0; i < node; i++) {
-	    if (!isVisited[i]) {
-	        dfs(i);
-	    }
-	}
-	
-	if (isCycle) {
-	    cout << "Cycle found" << endl;
-	} else {
-	    cout << "Cycle not found" << endl;
-	}
-	
 	return 0;
 }
